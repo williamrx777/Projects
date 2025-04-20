8b9d9f52b8bcadfe44445bfe4cea8c7e0c0e6a4b
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2025-04-20 16:48:01 -0300

    add:admin models

--- ramais/admin.py
+++ ramais/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Departamento)
+admin.site.register(Funcionario)
+admin.site.register(Lista_Ramais)
