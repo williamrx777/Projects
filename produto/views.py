06b9af2739b5e87d721b52584448b3e276d97ddf
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2025-05-01 14:49:42 -0300

    alter:produto

--- produto/views.py
+++ produto/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import *
+from .models import Produto
 # Create your views here.
 
 def produto(request):
-    produto = Produto.objects.all()
-    return render(request, 'produto.html', {produto: produto})
+    produtos = Produto.objects.all().order_by('-data_cadastro')  # opcional: limitar com [:12]
+    return render(request, 'produto.html', {produtos: produtos})
